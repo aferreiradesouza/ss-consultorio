fd63584e9a916d46f27799c15475c86eddf6c5bf
Rafael Souza <rsilveiradesouza@gmail.com>
2019-02-16 18:48:46 -0200

    ajuste de session

--- src/app/auth/registro/contato/contato.component.ts
+++ src/app/auth/registro/contato/contato.component.ts
@@ -26,7 +26,7 @@ export class ContatoComponent implements OnInit {
 
   gravar() {
       if (this.formContato.valid) {
-          this.sessionStorage.setJson('registro/contato', this.formContato.value);
+          this.sessionStorage.remove('registro/contato');
           this.proximo();
       }
   }
