107ed0cb8d69233bd07276b998cb4f3168afaad0
Rafael Souza <rsilveiradesouza@gmail.com>
2019-02-16 19:39:04 -0200

    ajuste do service

--- src/shared/service/ajax.service.ts
+++ src/shared/service/ajax.service.ts
@@ -3,44 +3,56 @@ import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AjaxService {
+  constructor(
+    private http: HttpClient,
+    public storageService: LocalStorageService
+  ) {}
 
-    constructor(
-        private http: HttpClient,
-        public storageService: LocalStorageService
-    ) { }
+  public async get<T>(
+    url: string,
+    params: { [param: string]: string | string[] } = {}
+  ) {
+    const token =
+      this.storageService.getJson('user') != null
+        ? this.storageService.getJson('user').token
+        : '111';
+    const headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+      Authorization: 'bearer ' + token
+    };
 
-    public async get<T>(url: string, params: { [param: string]: string | string[] } = {}) {
-        const headers = {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'bearer ' + this.storageService.getJson('user').token ? this.storageService.getJson('user').token : '111'
-        };
-        return this.http.get<T>(url, { params, headers }).toPromise();
-    }
-    public async post<T>(url: string, body: any = {}) {
+    return this.http.get<T>(url, { params, headers }).toPromise();
+  }
+  public async post<T>(url: string, body: any = {}) {
+    const token =
+      this.storageService.getJson('user') != null
+        ? this.storageService.getJson('user').token
+        : '111';
+    const options = {
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        Authorization: 'bearer ' + token
+      }
+    };
 
-        const options = {
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': 'bearer ' + this.storageService.getJson('user').token ? this.storageService.getJson('user').token : '111'
-            }
-        };
+    return this.http.post<T>(url, body, options).toPromise();
+  }
 
-        return this.http.post<T>(url, body, options).toPromise();
-    }
+  public async put<T>(url: string, body: any = {}) {
+    const token =
+      this.storageService.getJson('user') != null
+        ? this.storageService.getJson('user').token
+        : '111';
+    const options = {
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        Authorization: 'bearer ' + token
+      }
+    };
 
-    public async put<T>(url: string, body: any = {}) {
-
-        const options = {
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': 'bearer ' + this.storageService.getJson('user').token ? this.storageService.getJson('user').token : '111'
-            }
-        };
-
-        return this.http.put<T>(url, body, options).toPromise();
-    }
-
+    return this.http.put<T>(url, body, options).toPromise();
+  }
 }
