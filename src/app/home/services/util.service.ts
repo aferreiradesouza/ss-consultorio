30d883a2da91bb21c9f646221b9837581bd94828
Rafael Souza <rsilveiradesouza@gmail.com>
2019-03-07 03:02:59 -0300

    ajuste de ordenação e bug das consultas no dia;

--- src/app/home/services/util.service.ts
+++ src/app/home/services/util.service.ts
@@ -2,44 +2,61 @@ import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-
 export class UtilHomeService {
+  constructor() {}
 
-    constructor() { }
+  async formatarConsultas(dados: any[], tipo: string) {
+    const dataAtual = new Date();
+    const proximas = [];
+    const finalizadas = [];
 
-    async formatarConsultas(dados: any[], tipo: string) {
-        const dataAtual = new Date();
-        const proximas = [];
-        const finalizadas = [];
+    dados.forEach(f => {
+      const diaAtual =
+        moment(dataAtual).format('YYYY-MM-DD') +
+        'T' +
+        moment(dataAtual).format('HH:mm:ss');
+      const data = moment(f.data).format('YYYY-MM-DD');
+      const consulta = `${data}T${f.hora}:00`;
 
-        dados.forEach(f => {
-            const diaAtual = moment(dataAtual).format('YYYY-MM-DD') + 'T' + moment(dataAtual).format('LTS');
-            const data = moment(f.data).format('YYYY-MM-DD');
-            const consulta = `${data}T${f.hora}:00`;
+      if (diaAtual > consulta || f.statusConsulta.codigo === 'cancelado') {
+        finalizadas.push(f);
+      } else {
+        proximas.push(f);
+      }
+    });
 
-            if (diaAtual > consulta || f.statusConsulta.codigo === 'cancelado') {
-                finalizadas.push(f);
-            } else {
-                proximas.push(f);
-            }
-        });
-
-        if (tipo === 'proximas') {
-            proximas.sort(function (a, b) {
-                const dataA: any = new Date(new Date(a.data).getFullYear(), new Date(a.data).getMonth(), new Date(a.data).getDate());
-                const dataB: any = new Date(new Date(b.data).getFullYear(), new Date(b.data).getMonth(), new Date(b.data).getDate());
-                return dataA - dataB;
-            });
-        } else {
-            finalizadas.sort(function (a, b) {
-                const dataA: any = new Date(new Date(a.data).getFullYear(), new Date(a.data).getMonth(), new Date(a.data).getDate());
-                const dataB: any = new Date(new Date(b.data).getFullYear(), new Date(b.data).getMonth(), new Date(b.data).getDate());
-                return dataA - dataB;
-            });
-        }
-
-        return tipo === 'proximas' ? proximas : finalizadas;
+    if (tipo === 'proximas') {
+      proximas.sort(function(a, b) {
+        const dataA: any = new Date(
+          new Date(a.data).getFullYear(),
+          new Date(a.data).getMonth(),
+          new Date(a.data).getDate()
+        );
+        const dataB: any = new Date(
+          new Date(b.data).getFullYear(),
+          new Date(b.data).getMonth(),
+          new Date(b.data).getDate()
+        );
+        return dataA - dataB;
+      });
+    } else {
+      finalizadas.sort(function(a, b) {
+        const dataA: any = new Date(
+          new Date(a.data).getFullYear(),
+          new Date(a.data).getMonth(),
+          new Date(a.data).getDate()
+        );
+        const dataB: any = new Date(
+          new Date(b.data).getFullYear(),
+          new Date(b.data).getMonth(),
+          new Date(b.data).getDate()
+        );
+        return dataB - dataA;
+      });
     }
+
+    return tipo === 'proximas' ? proximas : finalizadas;
+  }
 }
